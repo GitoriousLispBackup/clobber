8040599a87f9944305cc82d9ac5da37ca1fd269e
derrida <michael@abstract.fm>
2010-10-27 04:05:53 -0400

    Moved to plain defobject definitions instead of the add-object abstraction.

--- objects.lisp
+++ objects.lisp
@@ -18,37 +18,39 @@
 (defobject unit (layer x y hp inventory))
 (defcontainer rucksack :slots 16)
 (defobject player (name direction str def) :inherit (unit))
+(defobject earth () :inherit (unit))
+(defobject stone () :inherit (earth))
+(defobject water () :inherit (earth))
+(defobject dirt () :inherit (earth))
+(defobject grass () :inherit (earth))
+(defobject fire () :inherit (earth))
+(defobject clay () :inherit (earth))
+(defobject air () :inherit (earth)))
+
 
 (defparameter *player* (make-instance 'player))
 (defparameter *world* (make-instance 'world))
-
-(let ((counter 0))
-  (defun add-object (name)  
-    (defobject name () :inherit (unit))
-    (setf (gethash counter (objects *world*)) name)
-    (push (make-instance name) (instances *world*))
-    (incf counter)))
-
-(defun initialize-object-table ()
-  (add-object 'earth)
-  (add-object 'stone)
-  (add-object 'water)
-  (add-object 'dirt)
-  (add-object 'grass)
-  (add-object 'fire)
-  (add-object 'clay)
-  (add-object 'air))
-
-(defun initialize-sprite-table ()
-  "Initializes the starting sprites."
-  (add-sprite 'empty)
-  (add-sprite 'player)
-  (add-sprite 'earth)
-  (add-sprite 'stone)
-  (add-sprite 'grass))
 
 (defun lookup-sprite (object)
   (sprite object))
 
 (defun lookup-object (id)
   (gethash id (objects *world*)))
+
+;; (let ((counter 0))
+;;   (defun add-object (name)  
+;;     (defobject name () :inherit (unit))
+;;     (setf (gethash counter (objects *world*)) name)
+;;     (push (make-instance name) (instances *world*))
+;;     (incf counter)))
+
+;; (defun initialize-object-table ()
+;;   (add-object 'earth)
+
+;; (defun initialize-sprite-table ()
+;;   "Initializes the starting sprites."
+;;   (add-sprite 'empty)
+;;   (add-sprite 'player)
+;;   (add-sprite 'earth)
+;;   (add-sprite 'stone)
+;;   (add-sprite 'grass))
